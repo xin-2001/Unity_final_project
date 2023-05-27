11379a13e569d5352ce3c3acd8ac3ca67ad4f3d0
poy4u <bery43191128@gmail.com>
2023-05-28 04:41:51 +0800

    <feat>測試地圖生成
    在Scripts裡面新增3個測試程式碼
    Test_CharacterMovement 假主角控制
    Test_Spawner 地圖生成位置
    Test_TriggerVolume 觸發地圖生成
    
    在地圖場景中建立假主角測試
    及建立用於地圖生成之Level

---
+++ Assets/Scripts/Test_Spawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Spawner : MonoBehaviour
+{
+    [Header("Levels")]
+    [SerializeField] private GameObject[] _levels;
+    [SerializeField] private float _stayTime = 15f;
+
+    // private
+    private bool _isSpawmed;
+    private float _elapsedTime;
+
+    public void SpawnLevel()
+    {
+        if (_levels == null) return;
+
+        int randomIndex = Random.Range(0, _levels.Length);
+        GameObject level = Instantiate(_levels[randomIndex], transform.position, transform.rotation) as GameObject;
+
+        // level.name = level.name.Replace("(Clone)", "").Trim();
+
+        _isSpawmed = true;
+    }
+
+    private void Update()
+    {
+        if(!_isSpawmed) return;
+
+        _elapsedTime += Time.deltaTime;
+
+        if(_elapsedTime >= _stayTime)
+        {
+            _elapsedTime = 0;
+            _isSpawmed = false;
+            Destroy(transform.parent.parent.gameObject);
+        }
+    }
+}
