15facb9cfab2de5f1ae8af8efa99b5a15b411ade
xin <77916095+xin-2001@users.noreply.github.com>
2023-05-30 03:02:29 +0800

    <feat>問題解決
    瓶子傾斜問題解決
    物件大小解決

---
+++ Assets/Scripts/bottle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bottle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(Vector3.right,25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
