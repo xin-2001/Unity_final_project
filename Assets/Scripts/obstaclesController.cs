09788c0f7eb000d33ca565749ea74276006b7683
AzusaFoT <fot531@gmail.com>
2023-05-29 23:51:46 +0800

    <feat> 撞到障礙物停止player行動
    撞到障礙物停止player行動

--- Assets/Scripts/obstaclesController.cs
+++ Assets/Scripts/obstaclesController.cs
@@ -5,23 +5,34 @@ using UnityEngine.Events;
 public class obstaclesController : MonoBehaviour
 {
     public UnityEvent<GameObject> OnEnter;
+    private carController car;
+    public int location;
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out carController obstacles))
         {
             //OnEnter.Invoke(obstacles.gameObject);
             Debug.Log(other.gameObject);
+            car.currentLocation = 3;
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-
+        // 獲取 carController.cs 的實例
+        car = GameObject.FindObjectOfType<carController>();
+        if (car != null)
+        {
+            int location = car.currentLocation;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (car != null)
+        {
+            int location = car.currentLocation;
+        }
     }
 }
