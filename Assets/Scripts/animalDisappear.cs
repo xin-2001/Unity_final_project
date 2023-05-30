faa802084d3481c9045e92a3227e40e31dc7a0ae
xin <77916095+xin-2001@users.noreply.github.com>
2023-05-30 18:12:29 +0800

    <feat>聖光製作
    動物只朝一方向，無法左往左，右往右，車子還沒製作

---
+++ Assets/Scripts/animalDisappear.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animalDisappear : MonoBehaviour
+{
+    //移动的的目标
+    GameObject target;
+    //是否可以移动
+    public bool isCanMove = false;
+    //移动的速度
+    public float speed = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCanMove)
+        {
+            //向外移動
+            int i=0;
+            if(i==0){
+                if(transform.position.x>=1){
+                    transform.Rotate(Vector3.up * 0.1f );
+                }else{
+                    transform.Rotate(Vector3.up * -0.1f );
+                }
+                
+                i=1;
+            }
+            
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            Destroy(gameObject,3f);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        //如果碰到的物体是玩家
+
+        if (other.tag.Equals("Player"))
+        {
+            //這裡要扣分
+            Destroy(gameObject,1f);
+        }
+    }
+}
---
+++ Assets/Scripts/toolDisappear.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toolDisappear : MonoBehaviour
+{
+    private bool isDisappear = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //如果玩家碰到吸铁石的话 就检测玩家周围的所有带有碰撞器的游戏对象
+        if (isDisappear)
+        {
+            //检测以玩家为球心半径是5的范围内的所有的带有碰撞器的游戏对象
+            Collider[] colliders = Physics.OverlapSphere(this.transform.position,2);
+            foreach (var item in colliders)
+            {
+                //如果是人
+                if (item.tag.Equals("animal"))
+                {
+                    //让金币的开始移动
+                    item.GetComponent<animalDisappear>().isCanMove = true;
+                }
+            }
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("onigiri"))
+        {
+            //设置玩家可以排斥動物
+            isDisappear = true;
+            //销毁吸铁石
+            Destroy(other.gameObject);
+        }
+    }
+}
